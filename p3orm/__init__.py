588f027ee8e9d72c16df17caaa1a5088fc0ead19
Rafal <stapinskirafal@gmail.com>
2022-02-21 17:24:52 -0500

    update __init__

---
+++ p3orm/__init__.py
@@ -0,0 +1,9 @@
+"""Utilitarian Python ORM for Postgres, backed by asyncpg, Pydantic, and PyPika"""
+
+__version__ = "0.2.3"
+
+from .core import *
+from .exceptions import *
+from .table import *
+from .types import *
+from .utils import *
