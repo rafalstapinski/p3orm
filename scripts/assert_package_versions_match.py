cae67e2814bff51b476352d69fcd4365c272b339
Rafal <stapinskirafal@gmail.com>
2022-02-24 22:19:02 -0500

    🧪 Add check for package version match

---
+++ scripts/assert_package_versions_match.py
@@ -0,0 +1,15 @@
+import tomli
+
+import p3orm
+
+
+def assert_versions_equal():
+
+    with open("pyproject.toml", "rb") as pyproject:
+        toml = tomli.load(pyproject)
+
+    assert p3orm.__version__ == toml["tool"]["poetry"]["version"], "pyproject.toml and p3orm.__version__ mismatch"
+
+
+if __name__ == "__main__":
+    assert_versions_equal()
