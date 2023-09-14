51bd87b095efed416b4a2cb7a0b42b6d12b2da6b
Rafal Stapinski <stapinskirafal@gmail.com>
2023-09-14 11:40:06 -0400

    add is_type to table

--- p3orm/table.py
+++ p3orm/table.py
@@ -94,13 +94,6 @@ class Table:
         if (not hasattr(cls, "__tablename__") or cls.__tablename__ is None) and cls.Meta.meta_table == False:
             raise MissingTablename(f"{cls.__name__} must define a __tablename__ property")
 
-        # if meta table, dont create model for this only end tables should
-        # consider moving to a model where there's a global mapping of table->class/fields/factory
-        # instead existing per table
-
-        # TODO: refactor this if/elif better
-        # TODO: figure out way to do isinstance type checks
-        # TODO: add test to check for the broken case
         elif cls.Meta.meta_table and not hasattr(cls, "__tablename__"):
             return
 
@@ -179,6 +172,10 @@ class Table:
     @classmethod
     def _field(cls, field_name: str) -> _PormField:
         return cls._field_map()[field_name]
+
+    @classmethod
+    def is_type(cls, obj: Any) -> bool:
+        return obj.__class__ == cls._model_factory
 
     #
     # Shortcuts
